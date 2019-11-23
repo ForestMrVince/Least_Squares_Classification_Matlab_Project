77adbf7263e9dd00d0bc2a26b6820a091a5ef041
ForestMrVince <ForestMrVince@users.noreply.github.com>
2019-11-23 15:07:11 +0800

    添加非线性函数
    求解函数；
    测试函数；

---
+++ Src/NonLinear_solver_test.m
@@ -0,0 +1,9 @@
+function [train_Z_right,train_Z_wrong,train_NZ_right,train_NZ_wrong,...
+    test_Z_right,test_Z_wrong,test_NZ_right,test_NZ_wrong]...
+    =NonLinear_solver_test(inputArg1,inputArg2)
+%UNTITLED2 此处显示有关此函数的摘要
+%   此处显示详细说明
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
