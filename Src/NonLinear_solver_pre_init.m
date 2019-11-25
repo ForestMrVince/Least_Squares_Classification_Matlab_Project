fcee0ac01e3a1dbe1cee7866345ccf83224d8a3f
ForestMrVince <ForestMrVince@users.noreply.github.com>
2019-11-26 00:38:43 +0800

    Update NonLinear_solver_pre_init.m

--- Src/NonLinear_solver_pre_init.m
+++ Src/NonLinear_solver_pre_init.m
@@ -5,19 +5,14 @@ function [beta_char,pre_fun] = NonLinear_solver_pre_init(train_images)
 window = waitbar(0,'非线性求解器预初始化中，请稍候！');
 
 beta_char = sym('b%d',[size(train_images,2),1]);
-Sigmoid_array = sym('S%d',[size(train_images,1),1]);
 pre_fun = sym('p%d',[size(train_images,1),1]);
 i_max = size(train_images,1);
-waitbar(1/(1+i_max+i_max));
-parfor i = 1:i_max
-    Sigmoid_array(i) = train_images(i,:)*beta_char;
-end
-waitbar((1+i_max)/(1+i_max+i_max));
+waitbar(1/(1+i_max));
 
-parfor j = 1:i_max
-    pre_fun(j) = sigmoid(Sigmoid_array(j));
+parfor i = 1:i_max
+    pre_fun(i) = sigmoid(train_images(i,:)*beta_char);
 end
-waitbar((1+i_max+i_max)/(1+i_max+i_max));%生成一个雅各布矩阵的行模板，然后向内带入元素
+waitbar((1+i_max)/(1+i_max));%生成一个雅各布矩阵的行模板，然后向内带入元素
 
 close(window);
 
