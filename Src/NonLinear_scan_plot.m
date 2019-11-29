b05115761fc0aaa488f490c8c91c62a3b503bd5e
ForestMrVince <Forest_Mu@users.noreply.github.com>
2019-11-29 20:37:37 +0800

    Update NonLinear_scan_plot.m

--- Src/NonLinear_scan_plot.m
+++ Src/NonLinear_scan_plot.m
@@ -5,19 +5,19 @@ function NonLinear_scan_plot(k_Max,Lambda,...
 train_error_rate = (train_NZ_wrong + train_Z_wrong)./(train_NZ_wrong + train_Z_wrong + train_NZ_right + train_Z_right);
 test_error_rate = (test_NZ_wrong + test_Z_wrong)./(test_NZ_wrong + test_Z_wrong + test_NZ_right + test_Z_right);
 
-heatmap(k_Max,Lambda,train_error_rate*1000);
+contourf(k_Max,Lambda,train_error_rate*1000);
 colorbar;
 title('训练集错误率等高线图（‰）');
-xlabel('最大循环次数 k_Max');
+xlabel('最大循环次数 k\_Max');
 ylabel('正则化系数 λ');
 set(gca,'fontsize',12,'fontname','宋体');
 grid on;
 
 figure;
-heatmap(k_Max,Lambda,test_error_rate*1000);
+contourf(k_Max,Lambda,test_error_rate*1000);
 colorbar;
 title('测试集错误率等高线图（‰）');
-xlabel('最大循环次数 k_Max');
+xlabel('最大循环次数 k\_Max');
 ylabel('正则化系数 λ');
 set(gca,'fontsize',12,'fontname','宋体');
 grid on;
